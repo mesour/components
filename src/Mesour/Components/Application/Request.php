5ced5814705d9674a960314dd8a3ea7aff95dad9
mesour <matous.nemec@gmail.com>
2016-12-21 13:27:24 +0100

    Fixed typo for PHP 7.1, added testing in php 7.1

--- src/Mesour/Components/Application/Request.php
+++ src/Mesour/Components/Application/Request.php
@@ -41,7 +41,7 @@ class Request
 	protected function getAllHeaders()
 	{
 		if (!function_exists('getallheaders')) {
-			$headers = '';
+			$headers = [];
 			foreach ($_SERVER as $name => $value) {
 				if (substr($name, 0, 5) == 'HTTP_') {
 					$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
